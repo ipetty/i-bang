45bf6d3502c7a571ea4d283c37fa55cb4179435d
kongchun <kcrens@gmail.com>
2014-09-16 10:13:02 +0800

    创建项目

---
+++ client/src/net/ipetty/ibang/android/main/MainActivity.java
@@ -0,0 +1,24 @@
+package net.ipetty.ibang.android.main;
+
+import net.ipetty.ibang.R;
+import net.ipetty.ibang.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		// getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
