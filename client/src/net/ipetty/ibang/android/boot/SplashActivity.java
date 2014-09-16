f0e5a17c2d6d4190945a062ea6d7309f49350ae3
kongchun <kcrens@gmail.com>
2014-09-16 14:52:56 +0800

    login

--- client/src/net/ipetty/ibang/android/boot/SplashActivity.java
+++ client/src/net/ipetty/ibang/android/boot/SplashActivity.java
@@ -1,6 +1,10 @@
 package net.ipetty.ibang.android.boot;
 
 import net.ipetty.ibang.R;
+import net.ipetty.ibang.android.core.util.AnimUtils;
+import net.ipetty.ibang.android.core.util.AppUtils;
+import net.ipetty.ibang.android.login.LoginActivity;
+import net.ipetty.ibang.android.main.MainActivity;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -10,6 +14,22 @@ public class SplashActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+
+		goLogin();
+
 	}
 
+	// 转向主界面
+	public void goMain() {
+		AppUtils.goTo(this, MainActivity.class);
+		AnimUtils.fadeInToOut(this);
+		finish();
+	}
+
+	// 转向登陆界面
+	public void goLogin() {
+		AppUtils.goTo(this, LoginActivity.class);
+		AnimUtils.fadeInToOut(this);
+		finish();
+	}
 }
---
+++ client/src/net/ipetty/ibang/android/login/LoginActivity.java
@@ -0,0 +1,124 @@
+package net.ipetty.ibang.android.login;
+
+import net.ipetty.ibang.R;
+import net.ipetty.ibang.android.core.util.AppUtils;
+import net.ipetty.ibang.android.main.MainActivity;
+
+import org.apache.commons.lang3.StringUtils;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.Selection;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+	private AutoCompleteTextView accountView;
+	private EditText passwordView;
+	private String account = null;
+	private String password = null;
+	private TextView toggleView = null;
+	private boolean psdDisplayFlg = false;
+	private ProgressDialog progressDialog;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+		TextView version = (TextView) this.findViewById(R.id.version_info);
+		String verStr = getResources().getString(R.string.app_version);
+		String VersionName = String.format(verStr, AppUtils.getAppVersionName(this));
+		version.setText(VersionName);
+
+		TextView btnReg = (TextView) this.findViewById(R.id.to_register);
+		btnReg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		progressDialog = new ProgressDialog(LoginActivity.this);
+		progressDialog.setIndeterminate(true);
+		progressDialog.setCancelable(false);
+		progressDialog.setMessage(getResources().getString(R.string.logining));
+
+		toggleView = (TextView) this.findViewById(R.id.login_toggle_password);
+		toggleView.setOnClickListener(togglePasswordClick);
+
+		accountView = (AutoCompleteTextView) this.findViewById(R.id.account);
+		passwordView = (EditText) this.findViewById(R.id.password);
+		// 登陆
+		View loginBtnView = (View) this.findViewById(R.id.button);
+		loginBtnView.setOnClickListener(loginOnClick);
+	}
+
+	// 登录
+	private final OnClickListener loginOnClick = new OnClickListener() {
+		@Override
+		public void onClick(View loginBtnView) {
+			if (!validateLogin()) {
+				return;
+			}
+
+			Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+			startActivity(intent);
+
+			// new UserLogin(LoginActivity.this).setListener(new
+			// LoginTaskListener(LoginActivity.this)).execute(account,
+			// password);
+		}
+	};
+
+	// 密码可见
+	private OnClickListener togglePasswordClick = new OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			int index = passwordView.getSelectionStart();
+			if (!psdDisplayFlg) {
+				// display password text, for example "123456"
+				// passwordView.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+				passwordView.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+				toggleView.setText(R.string.login_toggle_password_hide);
+			} else {
+				// hide password, display "."
+				// passwordView.setTransformationMethod(PasswordTransformationMethod.getInstance());
+				passwordView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+				toggleView.setText(R.string.login_toggle_password_show);
+			}
+			psdDisplayFlg = !psdDisplayFlg;
+			Editable etable = passwordView.getText();
+			Selection.setSelection(etable, index);
+		}
+	};
+
+	// 登录前校验
+	private boolean validateLogin() {
+		this.account = accountView.getText().toString();
+		this.password = passwordView.getText().toString();
+
+		if (StringUtils.isBlank(this.account)) {
+			accountView.requestFocus();
+			Toast.makeText(LoginActivity.this, R.string.login_empty_account, Toast.LENGTH_SHORT).show();
+			return false;
+		}
+		if (StringUtils.isBlank(this.password)) {
+			passwordView.requestFocus();
+			Toast.makeText(LoginActivity.this, R.string.login_empty_password, Toast.LENGTH_SHORT).show();
+			return false;
+		}
+
+		return true;
+	}
+
+}
