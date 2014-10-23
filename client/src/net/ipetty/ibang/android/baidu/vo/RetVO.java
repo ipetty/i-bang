96e78a670aab434abbf8cec70e5f02189eed89f8
jinghai <jinghai.xiao@gmail.com>
2014-10-23 17:35:51 +0800

    云存储接口定义

---
+++ client/src/net/ipetty/ibang/android/baidu/vo/RetVO.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.ipetty.ibang.android.baidu.vo;
+
+/**
+ *
+ * @author yneos
+ */
+public class RetVO {
+
+        private Long id;
+        private Integer status;
+        private String message;
+
+        /**
+         * @return the id
+         */
+        public Long getId() {
+                return id;
+        }
+
+        /**
+         * @param id the id to set
+         */
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        /**
+         * @return the status
+         */
+        public Integer getStatus() {
+                return status;
+        }
+
+        /**
+         * @param status the status to set
+         */
+        public void setStatus(Integer status) {
+                this.status = status;
+        }
+
+        /**
+         * @return the message
+         */
+        public String getMessage() {
+                return message;
+        }
+
+        /**
+         * @param message the message to set
+         */
+        public void setMessage(String message) {
+                this.message = message;
+        }
+
+}
