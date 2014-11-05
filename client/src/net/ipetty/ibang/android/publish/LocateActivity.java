0417d122d3c9de971d9bc6d4ca868b9b33258351
kongchun <kcrens@gmail.com>
2014-11-05 16:01:59 +0800

    定位

--- client/src/net/ipetty/ibang/android/publish/LocateActivity.java
+++ client/src/net/ipetty/ibang/android/publish/LocateActivity.java
@@ -5,12 +5,23 @@
  */
 package net.ipetty.ibang.android.publish;
 
+import net.ipetty.ibang.R;
+import net.ipetty.ibang.android.baidu.BaiduApiFactory;
+import net.ipetty.ibang.android.baidu.RetVO;
+import net.ipetty.ibang.android.core.ActivityManager;
+import net.ipetty.ibang.android.core.MyApplication;
+import net.ipetty.ibang.android.core.ui.BackClickListener;
+
+import org.apache.commons.lang3.StringUtils;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
@@ -33,219 +44,214 @@ import com.baidu.mapapi.search.geocode.GeoCoder;
 import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
 import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
 import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
-import net.ipetty.ibang.R;
-import net.ipetty.ibang.android.baidu.BaiduApiFactory;
-import net.ipetty.ibang.android.baidu.RetVO;
-import net.ipetty.ibang.android.core.ActivityManager;
-import net.ipetty.ibang.android.core.MyApplication;
-import net.ipetty.ibang.android.core.ui.BackClickListener;
-import org.apache.commons.lang3.StringUtils;
 
 /**
- *
+ * 
  * @author Administrator
  */
 public class LocateActivity extends Activity {
 
-    private String TAG = getClass().getSimpleName();
-    private ProgressDialog progressDialog;
-    //定位相关
-    private LocationClient mLocationClient;
-    private MapView mMapView;
-    private BaiduMap mBaiduMap;
-    private boolean isFirstLoc = true;// 是否首次定位
-    private Marker mMarkerA;
-    private GeoCoder mSearch = null; // 搜索模块，也可去掉地图模块独立使用
+	private String TAG = getClass().getSimpleName();
+	private ProgressDialog progressDialog;
+	// 定位相关
+	private LocationClient mLocationClient;
+	private MapView mMapView;
+	private BaiduMap mBaiduMap;
+	private boolean isFirstLoc = true;// 是否首次定位
+	private Marker mMarkerA;
+	private GeoCoder mSearch = null; // 搜索模块，也可去掉地图模块独立使用
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_locate);
-        ActivityManager.getInstance().addActivity(this);
-        /* action bar */
-        ImageView btnBack = (ImageView) this.findViewById(R.id.action_bar_left_image);
-        btnBack.setOnClickListener(new BackClickListener(this));
+	private TextView address;
 
-        // 地图初始化
-        mMapView = (MapView) findViewById(R.id.bmapView);
-        mBaiduMap = mMapView.getMap();
-        //设置地图缩放
-        MapStatusUpdate u2 = MapStatusUpdateFactory.zoomTo(16);
-        mBaiduMap.animateMapStatus(u2);
-        // 开启定位图层
-        mBaiduMap.setMyLocationEnabled(true);
-        mBaiduMap.setOnMapStatusChangeListener(new OnMapStateChange());
-        // 定位初始化
-        mLocationClient = ((MyApplication) getApplication()).mLocationClient;
-        //位置监听函数
-        mLocationClient.registerLocationListener(new MyLocationListener());
-        mLocationClient.start();
-        // 初始化搜索模块，注册事件监听
-        mSearch = GeoCoder.newInstance();
-        mSearch.setOnGetGeoCodeResultListener(new MyGeoCoderResultListener());
-        showDialog();
-        Log.d(TAG, "onCreate OK");
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_locate);
+		ActivityManager.getInstance().addActivity(this);
+		/* action bar */
 
-    @Override
-    protected void onPause() {
-        mMapView.onPause();
-        super.onPause();
-    }
+		ImageView btnBack = (ImageView) this.findViewById(R.id.action_bar_left_image);
+		TextView text = (TextView) this.findViewById(R.id.action_bar_title);
+		text.setText(R.string.title_activity_locate);
+		btnBack.setOnClickListener(new BackClickListener(this));
 
-    @Override
-    protected void onResume() {
-        mMapView.onResume();
-        super.onResume();
-    }
+		address = (TextView) this.findViewById(R.id.address);
 
-    @Override
-    protected void onStop() {
-        Log.d(TAG, "onStop");
-        //关闭定位组件
-        mLocationClient.stop();
-        // 关闭定位图层
-        mBaiduMap.setMyLocationEnabled(false);
-        mMapView.onDestroy();
-        mMapView = null;
-        super.onStop();
-    }
+		// 地图初始化
+		mMapView = (MapView) findViewById(R.id.bmapView);
+		mBaiduMap = mMapView.getMap();
+		// 设置地图缩放
+		MapStatusUpdate u2 = MapStatusUpdateFactory.zoomTo(16);
+		mBaiduMap.animateMapStatus(u2);
+		// 开启定位图层
+		mBaiduMap.setMyLocationEnabled(true);
+		mBaiduMap.setOnMapStatusChangeListener(new OnMapStateChange());
+		// 定位初始化
+		mLocationClient = ((MyApplication) getApplication()).mLocationClient;
+		// 位置监听函数
+		mLocationClient.registerLocationListener(new MyLocationListener());
+		mLocationClient.start();
+		// 初始化搜索模块，注册事件监听
+		mSearch = GeoCoder.newInstance();
+		mSearch.setOnGetGeoCodeResultListener(new MyGeoCoderResultListener());
+		showDialog();
+		Log.d(TAG, "onCreate OK");
+	}
 
-    private void showDialog() {
-        if (this.progressDialog == null) {
-            this.progressDialog = new ProgressDialog(this);
-        }
-        this.progressDialog.setIndeterminate(true);
-        this.progressDialog.setCancelable(true);
-        this.progressDialog.setMessage("正在确定您的位置");
-        this.progressDialog.show();
-    }
+	@Override
+	protected void onPause() {
+		mMapView.onPause();
+		super.onPause();
+	}
 
-    private void dismissDialog() {
-        this.progressDialog.dismiss();
-    }
+	@Override
+	protected void onResume() {
+		mMapView.onResume();
+		super.onResume();
+	}
 
-    //Geo查询
-    private class MyGeoCoderResultListener implements OnGetGeoCoderResultListener {
+	@Override
+	protected void onStop() {
+		Log.d(TAG, "onStop");
+		// 关闭定位组件
+		mLocationClient.stop();
+		// 关闭定位图层
+		mBaiduMap.setMyLocationEnabled(false);
+		mMapView.onDestroy();
+		mMapView = null;
+		super.onStop();
+	}
 
-        public void onGetGeoCodeResult(GeoCodeResult gcr) {
+	private void showDialog() {
+		if (this.progressDialog == null) {
+			this.progressDialog = new ProgressDialog(this);
+		}
+		this.progressDialog.setIndeterminate(true);
+		this.progressDialog.setCancelable(true);
+		this.progressDialog.setMessage("正在确定您的位置");
+		this.progressDialog.show();
+	}
 
-        }
+	private void dismissDialog() {
+		this.progressDialog.dismiss();
+	}
 
-        public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
-            if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
-                Toast.makeText(LocateActivity.this, "抱歉，未能找到结果", Toast.LENGTH_LONG)
-                    .show();
-                return;
-            }
-            Toast.makeText(LocateActivity.this, result.getAddress(),
-                Toast.LENGTH_LONG).show();
-        }
+	// Geo查询
+	private class MyGeoCoderResultListener implements OnGetGeoCoderResultListener {
 
-    }
+		public void onGetGeoCodeResult(GeoCodeResult gcr) {
 
-    //地图状态变化
-    private class OnMapStateChange implements OnMapStatusChangeListener {
+		}
 
-        public void onMapStatusChangeStart(MapStatus ms) {
-            Log.d(TAG, "onMapStatusChangeStart");
-        }
+		public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
+			if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+				//Toast.makeText(LocateActivity.this, "抱歉，未能找到结果", Toast.LENGTH_LONG).show();
+				address.setText("抱歉，未能找到结果");
+				return;
+			}
+			//Toast.makeText(LocateActivity.this, result.getAddress(), Toast.LENGTH_LONG).show();
+			address.setText(result.getAddress());
+		}
 
-        public void onMapStatusChange(MapStatus ms) {
-            Log.d(TAG, "onMapStatusChange");
-            //在地图中间增加Markerm BaiduMap.getMapStatus().target
-            BitmapDescriptor bd = BitmapDescriptorFactory
-                .fromResource(R.drawable.icon_gcoding);
-            if (mMarkerA == null) {
-                OverlayOptions ooA = new MarkerOptions().position(ms.target).icon(bd)
-                    .zIndex(9).draggable(false);
-                mMarkerA = (Marker) (mBaiduMap.addOverlay(ooA));
-            } else {
-                mMarkerA.setPosition(ms.target);
-            }
-        }
+	}
 
-        public void onMapStatusChangeFinish(MapStatus ms) {
-            Log.d(TAG, "onMapStatusChangeFinish");
-            // 反Geo搜索
-            mSearch.reverseGeoCode(new ReverseGeoCodeOption()
-                .location(ms.target));
-        }
-    }
+	// 地图状态变化
+	private class OnMapStateChange implements OnMapStatusChangeListener {
 
-    /**
-     * 实现实位回调监听
-     */
-    private class MyLocationListener implements BDLocationListener {
+		public void onMapStatusChangeStart(MapStatus ms) {
+			Log.d(TAG, "onMapStatusChangeStart");
+		}
 
-        @Override
-        public void onReceiveLocation(final BDLocation location) {
-            Log.d(TAG, "onReceiveLocation");
-            dismissDialog();
-            if (location == null || StringUtils.isBlank(location.getCity())) {
-                Toast.makeText(LocateActivity.this, "定位失败，请返回再次重试", Toast.LENGTH_LONG).show();
-                Log.d(TAG, "定位失败");
-                return;
-            }
-            //设置当前位置
-            MyLocationData locData = new MyLocationData.Builder()
-                .accuracy(location.getRadius())
-                // 此处设置开发者获取到的方向信息，顺时针0-360
-                .direction(100).latitude(location.getLatitude())
-                .longitude(location.getLongitude()).build();
-            mBaiduMap.setMyLocationData(locData);
-            if (isFirstLoc) {
-                isFirstLoc = false;
-                LatLng ll = new LatLng(location.getLatitude(),
-                    location.getLongitude());
-                MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
-                mBaiduMap.animateMapStatus(u);
-            }
+		public void onMapStatusChange(MapStatus ms) {
+			Log.d(TAG, "onMapStatusChange");
+			// 在地图中间增加Markerm BaiduMap.getMapStatus().target
+			BitmapDescriptor bd = BitmapDescriptorFactory.fromResource(R.drawable.icon_gcoding);
+			if (mMarkerA == null) {
+				OverlayOptions ooA = new MarkerOptions().position(ms.target).icon(bd).zIndex(9).draggable(false);
+				mMarkerA = (Marker) (mBaiduMap.addOverlay(ooA));
+			} else {
+				mMarkerA.setPosition(ms.target);
+			}
+		}
 
-            //Receive Location
-            StringBuilder sb = new StringBuilder(256);
-            sb.append("时间 : ");
-            sb.append(location.getTime());
-            sb.append("\n定位方式 : ");
-            sb.append(location.getLocType());
-            sb.append("\n纬度 : ");
-            sb.append(location.getLatitude());
-            sb.append("\n经度 : ");
-            sb.append(location.getLongitude());
-            sb.append("\n精度 : ");
-            sb.append(location.getRadius());
-            sb.append("\n省 : ");
-            sb.append(location.getProvince());
-            sb.append("\n市 : ");
-            sb.append(location.getCity());
-            sb.append("\n市编码 : ");
-            sb.append(location.getCityCode());
-            sb.append("\n区/县 : ");
-            sb.append(location.getDistrict());
-            sb.append("\n街道 : ");
-            sb.append(location.getStreet());
-            sb.append("\n门牌号 : ");
-            sb.append(location.getStreetNumber());
-            sb.append("\n详细地址 : ");
-            sb.append(location.getAddrStr());
-            Log.d(TAG, sb.toString());
-        }
+		public void onMapStatusChangeFinish(MapStatus ms) {
+			Log.d(TAG, "onMapStatusChangeFinish");
+			// 反Geo搜索
+			mSearch.reverseGeoCode(new ReverseGeoCodeOption().location(ms.target));
+		}
+	}
 
-    }
+	/**
+	 * 实现实位回调监听
+	 */
+	private class MyLocationListener implements BDLocationListener {
 
-    private void testLBS() {
-        String ak = "8691f5dfa58c9acccf7bb4c72e529382";
-        String geotable_id = "82284";
-        Integer coord_type = 3;
-        Double longitude = 121.39842;
-        Double latitude = 121.39842;
-        String title = "";
-        String tags = "";
-        String bid = "0000";
+		@Override
+		public void onReceiveLocation(final BDLocation location) {
+			Log.d(TAG, "onReceiveLocation");
+			dismissDialog();
+			if (location == null || StringUtils.isBlank(location.getCity())) {
+				//Toast.makeText(LocateActivity.this, "定位失败，请返回再次重试", Toast.LENGTH_LONG).show();
+				address.setText("定位失败，请手动选择");
+				Log.d(TAG, "定位失败");
+				return;
+			}
+			// 设置当前位置
+			MyLocationData locData = new MyLocationData.Builder().accuracy(location.getRadius())
+			// 此处设置开发者获取到的方向信息，顺时针0-360
+					.direction(100).latitude(location.getLatitude()).longitude(location.getLongitude()).build();
+			mBaiduMap.setMyLocationData(locData);
+			if (isFirstLoc) {
+				isFirstLoc = false;
+				LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+				MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
+				mBaiduMap.animateMapStatus(u);
+			}
 
-        RetVO ret = BaiduApiFactory.getBaiduApi().createPoi(ak, geotable_id, coord_type, longitude, latitude,
-            title, tags, bid);
-    }
+			// Receive Location
+			StringBuilder sb = new StringBuilder(256);
+			sb.append("时间 : ");
+			sb.append(location.getTime());
+			sb.append("\n定位方式 : ");
+			sb.append(location.getLocType());
+			sb.append("\n纬度 : ");
+			sb.append(location.getLatitude());
+			sb.append("\n经度 : ");
+			sb.append(location.getLongitude());
+			sb.append("\n精度 : ");
+			sb.append(location.getRadius());
+			sb.append("\n省 : ");
+			sb.append(location.getProvince());
+			sb.append("\n市 : ");
+			sb.append(location.getCity());
+			sb.append("\n市编码 : ");
+			sb.append(location.getCityCode());
+			sb.append("\n区/县 : ");
+			sb.append(location.getDistrict());
+			sb.append("\n街道 : ");
+			sb.append(location.getStreet());
+			sb.append("\n门牌号 : ");
+			sb.append(location.getStreetNumber());
+			sb.append("\n详细地址 : ");
+			sb.append(location.getAddrStr());
+			Log.d(TAG, sb.toString());
+		}
+
+	}
+
+	private void testLBS() {
+		String ak = "8691f5dfa58c9acccf7bb4c72e529382";
+		String geotable_id = "82284";
+		Integer coord_type = 3;
+		Double longitude = 121.39842;
+		Double latitude = 121.39842;
+		String title = "";
+		String tags = "";
+		String bid = "0000";
+
+		RetVO ret = BaiduApiFactory.getBaiduApi().createPoi(ak, geotable_id, coord_type, longitude, latitude, title,
+				tags, bid);
+	}
 
 }
--- client/src/net/ipetty/ibang/android/publish/SelectAddressActivity.java
+++ client/src/net/ipetty/ibang/android/publish/SelectAddressActivity.java
@@ -1,8 +1,13 @@
 package net.ipetty.ibang.android.publish;
 
 import net.ipetty.ibang.R;
+import net.ipetty.ibang.android.core.ActivityManager;
+import net.ipetty.ibang.android.core.ui.BackClickListener;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SelectAddressActivity extends Activity {
 
@@ -10,6 +15,16 @@ public class SelectAddressActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_select_address);
+		ActivityManager.getInstance().addActivity(this);
+
+		/* action bar */
+		ImageView btnBack = (ImageView) this.findViewById(R.id.action_bar_left_image);
+		TextView text = (TextView) this.findViewById(R.id.action_bar_title);
+		text.setText(R.string.title_activity_select_address);
+		btnBack.setOnClickListener(new BackClickListener(this));
+
+		Intent intent = new Intent(this, LocateActivity.class);
+		startActivity(intent);
 	}
 
 }
