4afa6078612cafce4ead098caa11e95df83c295f
luocanfeng <luocanfeng@gmail.com>
2014-11-10 12:25:28 +0800

    修改logincontroller

--- server/src/main/java/net/ipetty/ibang/web/controller/LoginController.java
+++ server/src/main/java/net/ipetty/ibang/web/controller/LoginController.java
@@ -1,21 +1,19 @@
 package net.ipetty.ibang.web.controller;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
+
+	/**
+	 * 跳转到登录界面
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(HttpServletResponse response, HttpServletRequest request, ModelMap model)
-			throws ServletException, IOException {
-		return "admin/login";
+	public ModelAndView toLogin() {
+		return new ModelAndView("admin/login");
 	}
+
 }
