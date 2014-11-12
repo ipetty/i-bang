5b3e8b5d6cd82611216fbd59b5712d70fc8a5a2f
luocanfeng <luocanfeng@gmail.com>
2014-11-12 11:02:33 +0800

    小调整

--- server/src/main/java/net/ipetty/ibang/web/controller/IdentityVerificationController.java
+++ server/src/main/java/net/ipetty/ibang/web/controller/IdentityVerificationController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import net.ipetty.ibang.admin.AdminConstants;
 import net.ipetty.ibang.model.IdentityVerification;
@@ -28,35 +29,42 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class IdentityVerificationController {
 
 	private static final int PAGE_SIZE = 20;
+
 	@Resource
 	private IdentityVerificationService identityVerificationService;
-	
+
 	@RequestMapping(value = "/verify", method = RequestMethod.GET)
-	public String verify(Model model, HttpServletRequest request, HttpServletResponse response,String currentPage) {
-		User user = (User)request.getSession().getAttribute(AdminConstants.SESSION_NAME);
-		//TODO:判断是否有权限登录
-		if(user == null){
+	public String verify(Model model, HttpServletRequest request, HttpServletResponse response, String currentPage) {
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			model.addAttribute("error", "您尚未登录，该界面需要登录后才能操作！");
 			return "/admin/login";
 		}
-		model.addAttribute("user",user);
-		
-		int cPage = 1;
-		if (StringUtils.isNotEmpty(currentPage)) {
-			cPage = Integer.parseInt(currentPage);
-		} else {
+		User user = (User) session.getAttribute(AdminConstants.SESSION_NAME);
+		if (user == null) {
+			model.addAttribute("error", "您尚未登录，该界面需要登录后才能操作！");
+			return "/admin/login";
+		}
+		if (!AdminConstants.ADMIN_USER_NAME.equals(user.getUsername())) {
+			model.addAttribute("error", "您不是管理员，该界面需要管理员身份才能操作！");
+			return "/admin/verify";
+		}
+
+		model.addAttribute("user", user);
+		if (StringUtils.isBlank(currentPage)) {
 			currentPage = "1";
 		}
-		//TODO:这里需要根据参数区分是全部还是部分
-		List<IdentityVerificationVO> listVerify = listVerifying(cPage,
-				PAGE_SIZE);
+		int cPage = Integer.parseInt(currentPage);
+		// TODO:这里需要根据参数区分是全部还是部分
+		List<IdentityVerificationVO> listVerify = listVerifying(cPage, PAGE_SIZE);
 		model.addAttribute("listVerify", listVerify);
 		model.addAttribute("currentPage", currentPage);
-		//TODO: 这里可能需要知道一共有多少页
+		// TODO: 这里可能需要知道一共有多少页
 		model.addAttribute("totalPage", 0);
-		
 
 		return "admin/verify";
 	}
+
 	/**
 	 * 审核
 	 */
@@ -69,7 +77,8 @@ public class IdentityVerificationController {
 	 * 获取待审核列表
 	 */
 	@RequestMapping(value = "/verify/listVerifying", method = RequestMethod.GET)
-	public @ResponseBody List<IdentityVerificationVO> listVerifying(int pageNumber, int pageSize) {
+	@ResponseBody
+	public List<IdentityVerificationVO> listVerifying(int pageNumber, int pageSize) {
 		List<IdentityVerification> identityVerifications = identityVerificationService.listVerifying(pageNumber,
 				pageSize);
 		return IdentityVerification.listToVoList(identityVerifications);
@@ -79,7 +88,8 @@ public class IdentityVerificationController {
 	 * 获取身份审核列表
 	 */
 	@RequestMapping(value = "/verify/listAll", method = RequestMethod.GET)
-	public @ResponseBody List<IdentityVerificationVO> list(int pageNumber, int pageSize) {
+	@ResponseBody
+	public List<IdentityVerificationVO> list(int pageNumber, int pageSize) {
 		List<IdentityVerification> identityVerifications = identityVerificationService.list(pageNumber, pageSize);
 		return IdentityVerification.listToVoList(identityVerifications);
 	}
--- server/src/main/java/net/ipetty/ibang/web/controller/LoginController.java
+++ server/src/main/java/net/ipetty/ibang/web/controller/LoginController.java
@@ -1,29 +1,21 @@
 package net.ipetty.ibang.web.controller;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import net.ipetty.ibang.admin.AdminConstants;
 import net.ipetty.ibang.exception.BusinessException;
-import net.ipetty.ibang.model.IdentityVerification;
 import net.ipetty.ibang.model.User;
-import net.ipetty.ibang.service.IdentityVerificationService;
 import net.ipetty.ibang.service.UserService;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
-public class LoginController{
+public class LoginController {
 
 	@Resource
 	private UserService userService;
@@ -43,26 +35,25 @@ public class LoginController{
 	 * 登陆
 	 */
 	@RequestMapping(value = "/web/login", method = RequestMethod.POST)
-	public @ResponseBody AjaxMessage<?> login(String username, String password) {
-		//logger.debug("login with loginName={}", username);
-		//Assert.hasText(username, "登录名不能为空");
-		//Assert.hasText(password, "密码不能为空");
+	@ResponseBody
+	public AjaxMessage<?> login(String username, String password) {
+		// logger.debug("login with loginName={}", username);
+		// Assert.hasText(username, "登录名不能为空");
+		// Assert.hasText(password, "密码不能为空");
 		AjaxMessage<?> result = AjaxMessage.OPERATION_SUCCESS_MESSAGE;
-		try{
+		try {
 			User user = userService.login(username, password); // 未发生异常则已登录成功
 			HttpSession session = request.getSession(true);
 			session.setAttribute(AdminConstants.SESSION_NAME, user);
-		}catch(BusinessException e){
+		} catch (BusinessException e) {
 			result = new AjaxMessage<String>(e);
 		}
 		return result;
 	}
-	
 
-	
 	@RequestMapping(value = "/web/logout", method = RequestMethod.GET)
 	public String logout() {
-		request.getSession().removeAttribute(AdminConstants.SESSION_NAME);
+		request.getSession(true).removeAttribute(AdminConstants.SESSION_NAME);
 		return "/admin/login";
 	}
 
