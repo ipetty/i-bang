037335aa0cc8cc76bb88bd239f5b7eafea2a6e2b
jinghai <jinghai.xiao@gmail.com>
2014-11-14 18:32:52 +0800

    百度API测试(80%)

--- server/src/test/java/net/ipetty/ibang/baidu/api/BaiduApiTest.java
+++ server/src/test/java/net/ipetty/ibang/baidu/api/BaiduApiTest.java
@@ -4,6 +4,7 @@ import net.ipetty.ibang.baidu.BaiduApi;
 import net.ipetty.ibang.baidu.BaiduApiFactory;
 import net.ipetty.ibang.baidu.vo.GeoGetByAdressRetVO;
 import net.ipetty.ibang.baidu.vo.GeoGetByXYRetVO;
+import net.ipetty.ibang.baidu.vo.PoiRetVO;
 import net.ipetty.ibang.baidu.vo.RetVO;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.junit.Test;
@@ -23,13 +24,19 @@ public class BaiduApiTest {
     @Test
     public void testAll() {
         BaiduApi baiduApi = BaiduApiFactory.getBaiduApi();
-
+        //创建POI
         RetVO ret = baiduApi.lbsCreatePoi(BaiduApiFactory.ak, BaiduApiFactory.lbsTableId, BaiduApiFactory.coordTypeValue, 121.487899, 31.249162,
             "测试Title", "测试,LBS", "测试Bid");
-        baiduApi.lbsDeletePoi(BaiduApiFactory.ak, BaiduApiFactory.lbsTableId, ret.getId());
+        ret = baiduApi.lbsUpdatePoi(ret.getId(), BaiduApiFactory.ak, BaiduApiFactory.lbsTableId, BaiduApiFactory.coordTypeValue, 121.487899, 31.249162,
+            "测试Title-Update", "测试,LBS", "测试Bid");
 
+        PoiRetVO poiRetVO = baiduApi.lbsGetPoi(ret.getId(), BaiduApiFactory.ak, BaiduApiFactory.lbsTableId);
+        //删除POI
+        //ret = baiduApi.lbsDeletePoi(BaiduApiFactory.ak, BaiduApiFactory.lbsTableId, ret.getId());
+        //获取经纬度
         GeoGetByAdressRetVO geoGetByAdressRetVO = baiduApi.geoGetByAdress(BaiduApiFactory.ak, "上海市", "上海市", "json");
         logger.debug(ToStringBuilder.reflectionToString(geoGetByAdressRetVO.getResult()));
+        //获取详细地址
         GeoGetByXYRetVO geoGetByXYRetVO = baiduApi.geoGetByXY(BaiduApiFactory.ak, "31.249162,121.487899", "json");
         logger.debug(ToStringBuilder.reflectionToString(geoGetByXYRetVO.getResult()));
     }
